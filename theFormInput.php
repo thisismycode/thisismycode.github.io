cc5a4d3feda75f310c7fe84c92337b6b5ee970f0
john <john@presario-laptop>
2015-08-12 14:05:32 +1000

    added JS and CSS to local directory started work on the PHP for processing

---
+++ theFormInput.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<?php
+//require_once("dv.php");
+echo $_POST['userName'];
+$theUsername = $_POST['userName'];
+$theUserEmail = $_POST['userEmail'];
+$theRating = $_POST['rating'];
+$theUserFeedback = $_POST['userFeedback'];
+
+
+
+
+//echo "Hello<br />";
+//echo $_POST["rating"];
+//echo $_POST["userName"];
+//echo $_POST["userEmail"];
+//echo "<br />bye<br />";
+//print_r($_POST);
+//
+
+?>
+</body>
+</html>
