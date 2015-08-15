3bea086b5554258a32d4b58437a4075d5f0fe7ab
john <john@presario-laptop>
2015-08-15 10:55:23 +1000

    debugging change to connections

--- Connections/mysql.php
+++ Connections/mysql.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
+echo "Connected successfully";
 
 
 ?>
